d4acfc4067878792abd08bb95f125dfa2837be3b
Loi Tran <lvt15@my.fsu.edu>
2018-04-29 11:30:19 +0800

    Add Scenes using react router flux

--- src/Router.js
+++ src/Router.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import { Scene, Router } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
+import EmployeeList from './components/EmployeeList';
 
 const RouterComponent = () => {
   return (
     <Router>
-      <Scene
-        key="login"
-        component={LoginForm}
-        title="Please login"
-      />
+      <Scene key='root'>
+        <Scene
+          key='login'
+          component={LoginForm}
+          title='Please login'
+        />
+        <Scene
+          key='employeeList'
+          component={EmployeeList}
+          title='Employees'
+        />
+      </Scene>
     </Router>
   );
 };
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -45,35 +45,37 @@ class LoginForm extends Component {
 
   render() {
     return (
-      <Card>
-        <CardSection>
-          <Input
-            label="Email"
-            placeholder="email@gmail.com"
-            onChangeText={this.onEmailChange.bind(this)}
-            value={this.props.email}
-          />
-        </CardSection>
+      <View style={{ marginTop: 75 }}>
+        <Card>
+          <CardSection>
+            <Input
+              label="Email"
+              placeholder="email@gmail.com"
+              onChangeText={this.onEmailChange.bind(this)}
+              value={this.props.email}
+            />
+          </CardSection>
 
-        <CardSection>
-          <Input
-            secureTextEntry
-            label="Password"
-            placeholder="password"
-            onChangeText={this.onPasswordChange.bind(this)}
-            value={this.props.password}
-          />
-        </CardSection>
+          <CardSection>
+            <Input
+              secureTextEntry
+              label="Password"
+              placeholder="password"
+              onChangeText={this.onPasswordChange.bind(this)}
+              value={this.props.password}
+            />
+          </CardSection>
 
-        {this.renderError()}
-        {/* <Text style={errorTextStyle}>
-          {this.props.error}
-        </Text> */}
+          {this.renderError()}
+          {/* <Text style={errorTextStyle}>
+            {this.props.error}
+          </Text> */}
 
-        <CardSection>
-          {this.renderButton()}
-        </CardSection>
-      </Card>
+          <CardSection>
+            {this.renderButton()}
+          </CardSection>
+        </Card>
+      </View>
     );
   }
 }
